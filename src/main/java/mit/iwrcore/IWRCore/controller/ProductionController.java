a9b33e17a73c616e21730681b8bddf6de0cc48b0
kim hyerim <kimhyyefla@gmail.com>
2024-08-01 14:17:41 +0900

    수정중

--- src/main/java/mit/iwrcore/IWRCore/controller/ProductionController.java
+++ src/main/java/mit/iwrcore/IWRCore/controller/ProductionController.java
@@ -8,10 +8,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/production")
 public class ProductionController {
     @GetMapping("/list_manufacture")
-        public void list_manufacture() {
+    public void list_manufacture() {
     }
+
+    @GetMapping("/new_manufacture")
+    public void new_manufacture() {
+    }
+
+    @GetMapping("/modify_manufacture")
+    public void modify_manufacture() {
+    }
+
+    @GetMapping("/manufacture")
+    public void manufacture() {
+    }
+
     @GetMapping("/list_newProduct")
-        public void list_newProduct() {
+    public void list_newProduct() {
     }
 
 }
