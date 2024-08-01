746eab7cab80328526e8a53b535545fd7dfc7bf6
kim hyerim <kimhyyefla@gmail.com>
2024-08-01 15:16:13 +0900

    수정중

---
+++ src/main/java/mit/iwrcore/IWRCore/controller/ContractController.java
@@ -0,0 +1,15 @@
+package mit.iwrcore.IWRCore.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/contract")
+public class ContractController {
+    @GetMapping("/new_contract")
+    public void new_contract(){
+    }
+    
+}
+
