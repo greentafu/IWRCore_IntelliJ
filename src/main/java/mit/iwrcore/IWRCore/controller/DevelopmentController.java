ce16109aac72618e6a045878bc78a41707d1a7e1
woosik <podlws@nate.com>
2024-08-01 12:41:30 +0900

    Merge branch 'main' of https://github.com/greentafu/IWRCore_IntelliJ

--- src/main/java/mit/iwrcore/IWRCore/controller/DevelopmentController.java
+++ src/main/java/mit/iwrcore/IWRCore/controller/DevelopmentController.java
@@ -7,15 +7,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/development")
 public class DevelopmentController {
-    @GetMapping("input_dev")
+    @GetMapping("/input_dev")
     public void input_dev(){
 
     }
-    @GetMapping("list_dev")
+    @GetMapping("/list_dev")
     public void list_dev(){
 
     }
-    @GetMapping("detail_dev")
+    @GetMapping("/detail_dev")
     public void detail_dev(){
 
     }
