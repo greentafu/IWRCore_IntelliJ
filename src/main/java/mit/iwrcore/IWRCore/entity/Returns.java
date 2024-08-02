e3b2f259d4b271ab0de827792f8bd82543181f96
woosik <podlws@nate.com>
2024-08-02 12:27:49 +0900

    Merge branch 'main' of https://github.com/greentafu/IWRCore_IntelliJ

---
+++ src/main/java/mit/iwrcore/IWRCore/entity/Returns.java
@@ -0,0 +1,30 @@
+package mit.iwrcore.IWRCore.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@ToString(exclude = {"shipment", "writer"})
+public class Returns extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long reNO;                   // 반품 번호
+
+    private String reDetail;            // 반품 내용
+    private String whyRe;               // 반품 이유
+    private String bGo;                 // 비고
+    private String filename;            // 파일
+    private String email;               // 담당자 이메일
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "shipment_id")  // 외래 키 컬럼 이름
+    private Shipment shipment;          // 관련된 Shipment
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "writer_id")    // 외래 키 컬럼 이름
+    private Member writer;              // 작성자
+}
---
+++ src/main/java/mit/iwrcore/IWRCore/entity/Shipment.java
@@ -0,0 +1,35 @@
+package mit.iwrcore.IWRCore.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@ToString(exclude = {"writer", "invoice", "returns", "order"})
+public class Shipment extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long shipNO;                // 출고 번호
+
+    private Long shipNum;               // 출고 수량
+    private LocalDateTime receipt;      // 입고 일
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "writer_id")    // 외래 키 컬럼 이름
+    private Member writer;              // 작성자
+
+    @OneToOne(fetch = FetchType.LAZY)
+    private Returns returns;            // 관련된 Returns
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "invoice_id")   // 외래 키 컬럼 이름
+    private Invoice invoice;            // 관련된 Invoice
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id")
+    private Order order;                // 관련된 Order
+}
