9842879a7078bdab9f3bea3fea4f933c52bdc76f
kim hyerim <kimhyyefla@gmail.com>
2024-08-02 12:40:08 +0900

    엔티티만드는중

--- src/main/java/mit/iwrcore/IWRCore/entity/Request.java
+++ src/main/java/mit/iwrcore/IWRCore/entity/Request.java
@@ -14,6 +14,7 @@ import java.time.LocalDate;
 @ToString
 public class Request extends BaseEntity {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long requstCode;
     private LocalDate eventDate;
     private String text;
