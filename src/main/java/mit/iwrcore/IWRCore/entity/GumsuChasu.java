73657c2e2a118dc82d419b36ca151d9edeed6627
kim hyerim <kimhyyefla@gmail.com>
2024-08-19 08:51:47 +0900

    풀

--- src/main/java/mit/iwrcore/IWRCore/entity/GumsuChasu.java
+++ src/main/java/mit/iwrcore/IWRCore/entity/GumsuChasu.java
@@ -13,6 +13,7 @@ import java.time.LocalDateTime;
 @ToString(exclude = {"writer", "gumsu"})
 public class GumsuChasu extends BaseEntity {
 
+    @Id
     private Long gumsuNum;      // 검수량
     private LocalDateTime gumsu1; // 검수일
 
