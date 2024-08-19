32f6c778e69c7c7fe9cc2e9adffcebcab44eebc7
woosik <podlws@nate.com>
2024-08-19 18:08:12 +0900

    Merge branch 'main' of https://github.com/greentafu/IWRCore_IntelliJ

--- src/main/java/mit/iwrcore/IWRCore/controller/JodalController.java
+++ src/main/java/mit/iwrcore/IWRCore/controller/JodalController.java
@@ -5,12 +5,16 @@ import mit.iwrcore.IWRCore.security.dto.MaterDTO.MaterCodeListDTO;
 import mit.iwrcore.IWRCore.security.dto.PageDTO.PageRequestDTO;
 import mit.iwrcore.IWRCore.security.dto.PageDTO.PageRequestDTO2;
 import mit.iwrcore.IWRCore.security.dto.ProDTO.ProCodeListDTO;
+import mit.iwrcore.IWRCore.security.dto.StructureDTO;
 import mit.iwrcore.IWRCore.security.service.*;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/jodal")
@@ -19,6 +23,8 @@ public class JodalController {
 
     private final JodalPlanService jodalPlanService;
     private final ContractService contractService;
+    private final StructureService structureService;
+
 
     @GetMapping("/list_jodal")
     public void list_jodal(PageRequestDTO pageRequestDTO, PageRequestDTO2 pageRequestDTO2, Model model){
@@ -26,8 +32,17 @@ public class JodalController {
         model.addAttribute("yesPlan_list", contractService.yesJodalplanMaterial(pageRequestDTO2));
     }
     @GetMapping("/jodal_ready")
-    public void jodal_ready(){
+    public void jodalReady(@RequestParam("manufactureCode") Long manuCode, Model model) {
+        // manuCode를 통해 관련 StructureDTO 데이터를 조회
+        List<StructureDTO> structureList = structureService.findByProduct_ManuCode(manuCode);
+
+        // 모델에 데이터 추가
+        model.addAttribute("structureList", structureList);
+        model.addAttribute("manuCode", manuCode); // manuCode를 뷰에 전달
+
+
     }
+
     @GetMapping("/modify_jodal")
     public void modify_jodal(){
     }
